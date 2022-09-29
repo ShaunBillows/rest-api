0c49373a1a4d022386ea738b0a68d8357c3dbf5c
ShaunBillows <billows.shaun@gmail.com>
2022-09-29 13:58:34 +0100

    deploy

--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ const express = require("express")
 // add routes and controllers
 const userRouter = require("./user/routes");
 const app = express();
+const port = process.env.PORT || 5000;
 
 //add relevant routes and controllers to app before listen runs
 app.use(express.json()); //Tell entire server that it will always recieve JSON, and to always send back JSON
 app.use(cors());
 app.use(userRouter);
 
-app.listen(5001, () => {
-    console.log("Listening on port 5001.");
+app.listen(port, () => {
+    console.log(`Listening on port ${PORT}.`);
   });
